4c78394c9e70cb5378e9603ac80e9566a98b36f2
Agnieszka <agulec113@wp.pl>
2016-03-06 16:49:34 +0100

    Process Class created

--- SO_Lab1/src/Process.java
+++ SO_Lab1/src/Process.java
@@ -2,4 +2,48 @@
 public class Process {
 	// this class will contain process values
 	//Aga will create this class
+	
+	
+	//ar = Arrival Time (czas zg³oszenia) bt = Burst Time (d³ugoœæ fazy procesora) id = Number (numer) hm = How much (ile wykonano) isDone (czy wykonane)
+	private final int ar; 
+	private final int bt;
+	private final int id;
+	private int hm;
+	private boolean isDone;
+	
+	public Process(int ar, int bt, int id){
+		this.ar = ar;
+		this.bt = bt;
+		this.id = id;
+		this.hm = 0;
+		isDone = false;
+	}
+
+	public int getHm() {
+		return hm;
+	}
+
+	public int getAr() {
+		return ar;
+	}
+
+	public int getBt() {
+		return bt;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void increase(int hm) {
+		this.hm += hm;
+	}
+	
+	public boolean isDone() {
+		if (hm >= bt)
+			isDone = true;
+		
+		return isDone;
+	}
+	
 }
